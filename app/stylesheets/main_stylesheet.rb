e6e31b72b0641707814532946df75338b4b11e7f
Alexander Ross <izepax@gmail.com>
2014-05-25 18:27:41 +0200

    Adds search form

--- app/stylesheets/main_stylesheet.rb
+++ app/stylesheets/main_stylesheet.rb
@@ -6,7 +6,7 @@ class MainStylesheet < ApplicationStylesheet
   end
 
   def root_view(st)
-    st.background_color = color.white
+    st.background_color = color.light_gray
   end
 
   def hello_world(st)
@@ -16,6 +16,28 @@ class MainStylesheet < ApplicationStylesheet
     st.text = 'Hello World'
     st.color = color.battleship_gray
     st.font = font.medium
+  end
+
+  def search_label(st)
+    st.frame = {l: 10, t: 100, w: app_width - 20, h: 40}
+    st.text_alignment = :center
+    st.text = 'Find'
+    st.color = color.white
+    st.font = font.large
+  end
+
+  def query(st)
+    st.frame = {l: 10, t: 150, w: app_width - 20, h: 40}
+    st.background_color = color.white
+    st.font = font.large
+  end
+
+  def submit_button(st)
+    st.frame = {l: 10, t: 200, w: app_width - 20, h: 40}
+    st.background_color = color.blue
+    st.text = 'Go!'
+    st.color = color.white
+    st.font = font.large
   end
 
 end
